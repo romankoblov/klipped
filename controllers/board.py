cc32381bf40f96cb5cb3d58c5c889fd5e67ca092
Roman Koblov <penpen938@me.com>
2010-12-18 19:47:56 +0300

    Some fixes

--- controllers/board.py
+++ controllers/board.py
@@ -10,8 +10,17 @@ import tornado.web
 from brukva import adisp
 from functools import partial
 import simplejson
+import time
+import datetime
 
 async = partial(adisp.async, cbname='callbacks')
+
+def timestamp(datetime_obj):
+    """ Convert datetime object to timestamp """
+    if type(datetime_obj) is datetime.datetime:
+        return int(time.mktime(datetime_obj.timetuple()))
+    else:
+        return int(datetime_obj)
 
 class BoardHandler(tornado.web.RequestHandler):
     """ Board handler """
@@ -19,9 +28,10 @@ class BoardHandler(tornado.web.RequestHandler):
     @adisp.process
     def get(self, board, format='html'):
         """ Returns threads """
-        (_, thread_count) = yield async(self.application.redis.llen)("board:{board}:threads".format(board=board))
-        (_, threads) = yield async(self.application.redis.lrange)("board:{board}:threads".format(board=board), 0, 10)
-        self.render("board.html", title="My title", threads=threads, thread_count=thread_count)
+        (_, threads) = yield async(self.application.redis.zrevrange)("board:{board}:threads".format(board=board), 0, 10, with_scores=False)
+        if not threads:
+            threads = []
+        self.render("board.html", title="My title", threads=threads)
 
     @tornado.web.asynchronous
     @adisp.process
@@ -36,7 +46,7 @@ class BoardHandler(tornado.web.RequestHandler):
         (_, key) = yield async(self.application.redis.incr)("board:{board}:id".format(board=board))
         print key
         data['id'] = key
-        self.application.redis.rpush("board:{board}:threads".format(board=board), key)
+        self.application.redis.zadd("board:{board}:threads".format(board=board), timestamp(datetime.datetime.now()), key)
         self.application.redis.incr("thread:{board}:{thread}:posts_counts".format(board=board, thread=key))
         data_post = {}
         self.application.redis.set("post:{board}:{thread}:{post_id}:json".format(board=board, thread=key, post_id=key), simplejson.dumps(data))
--- controllers/thread.py
+++ controllers/thread.py
@@ -11,8 +11,15 @@ import tornado.web
 from brukva import adisp
 from functools import partial
 import simplejson
-
+import time, datetime
 async = partial(adisp.async, cbname='callbacks')
+
+def timestamp(datetime_obj):
+    """ Convert datetime object to timestamp """
+    if type(datetime_obj) is datetime.datetime:
+        return int(time.mktime(datetime_obj.timetuple()))
+    else:
+        return int(datetime_obj)
 
 class ThreadHandler(tornado.web.RequestHandler):
     """ Thread handler """
@@ -39,7 +46,14 @@ class ThreadHandler(tornado.web.RequestHandler):
         (_, key) = yield async(self.application.redis.incr)("board:{board}:id".format(board=board))
         print key
         data['id'] = key
+        data['date'] = timestamp(datetime.datetime.now())
+        # Update thread post counts
         self.application.redis.incr("thread:{board}:{thread}:posts_counts".format(board=board, thread=thread))
+        # Adding post to db
         self.application.redis.set("post:{board}:{thread}:{post_id}:json".format(board=board, thread=thread, post_id=key), simplejson.dumps(data))
+        # Adding post to thread
         self.application.redis.rpush("thread:{board}:{thread}:posts".format(board=board, thread=thread), key)
+        # Up thread
+        self.application.redis.zrem("board:{board}:threads".format(board=board), thread)
+        self.application.redis.zadd("board:{board}:threads".format(board=board), timestamp(datetime.datetime.now()), thread)
         self.redirect('/{board}/{thread}.html'.format(board=board, thread=thread))
